6143038d0eb7317932e67ef3c8e5dd9bc39c7687
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-04-22 18:03:56 +0300

    Measures of Frequency

--- ExploratoryDataAnalysis.R
+++ ExploratoryDataAnalysis.R
@@ -45,3 +45,49 @@ head(attrition_data)
 
 # Open the dataset in a viewer window
 View(attrition_data)
+
+#Measures of Frequency
+# Measures of frequency for Attrition variable
+table(attrition_data$Attrition)
+
+# Measures of frequency for BusinessTravel variable
+table(attrition_data$BusinessTravel)
+
+# Measures of frequency for Department variable
+table(attrition_data$Department)
+
+# Measures of frequency for Education variable
+table(attrition_data$Education)
+
+# Measures of frequency for EducationField variable
+table(attrition_data$EducationField)
+
+# Measures of frequency for EnvironmentSatisfaction variable
+table(attrition_data$EnvironmentSatisfaction)
+
+# Measures of frequency for Gender variable
+table(attrition_data$Gender)
+
+# Measures of frequency for JobRole variable
+table(attrition_data$JobRole)
+
+# Measures of frequency for MaritalStatus variable
+table(attrition_data$MaritalStatus)
+
+# Measures of frequency for Over18 variable
+table(attrition_data$Over18)
+
+# Measures of frequency for OverTime variable
+table(attrition_data$OverTime)
+
+# Measures of frequency for PerformanceRating variable
+table(attrition_data$PerformanceRating)
+
+# Measures of frequency for RelationshipSatisfaction variable
+table(attrition_data$RelationshipSatisfaction)
+
+# Measures of frequency for StockOptionLevel variable
+table(attrition_data$StockOptionLevel)
+
+# Measures of frequency for WorkLifeBalance variable
+table(attrition_data$WorkLifeBalance)
